fcccf14a3f813a411506be7a008882908edc0d12
Enoch <mrenoch@gmail.com>
2020-08-05 14:58:49 -0600

    init

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,14 @@ fn main() {
     // let my_led = Pin::new(26);
     let step_pin = Pin::new(step);
     let dir_pin = Pin::new(dir);
-
-    step_pin.set_direction(Direction::Out).unwrap();
-    dir_pin.set_direction(Direction::Out).unwrap();
+    dir_pin.with_exported({
+        dir_pin.set_direction(Direction::Out).unwrap();
+    });
 
     step_pin.with_exported(move|| {
+
+        step_pin.set_direction(Direction::Out).unwrap();
+
 
         for _x in 0..200 {
             step_pin.set_value(1).unwrap();
