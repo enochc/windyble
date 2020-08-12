212063c92e2883528d899f83a85567e84dfae1ed
Enoch <mrenoch@gmail.com>
2020-08-12 09:36:08 -0600

    motor

--- src/motor.rs
+++ src/motor.rs
@@ -41,9 +41,10 @@ impl Motor {
        returns microseconds, 1,000,000 in a sec
     */
     pub fn set_speed(&mut self, val:u64) {
-        println!("<<<<<< set speed")
+
         let speed = (((SPEED_MAX - SPEED_MIN)/ 100) * val) + SPEED_MIN;
-        self.step_duration.store(u64::from(Duration::from_micros(speed).subsec_micros()), Ordering::SeqCst);
+        println!("<<<<<< set speed {}, {}", val, speed);
+        self.step_duration.store(speed, Ordering::SeqCst);
     }
     pub fn new(step_pin: MyPin, dir_pin: MyPin, is_test:bool) -> Motor {
         let duration = if is_test {
